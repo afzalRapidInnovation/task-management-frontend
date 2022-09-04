8d5b6e5e4073849cb6fdf79943b058267bd28dd0
afzalRapidInnovation <afzalRapidInnovation@users.noreply.github.com>
2022-09-04 17:26:01 +0000

    deploy: 6931a8d12a43b868d9a50b2d554b141626f3b813

---
+++ precache-manifest.bcea44305c6b25baa083e6b158aa20da.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7940a0513cd28b6ba125e48fb6a20747",
+    "url": "https://afzalRapidInnovation.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "ed6f2b8ce9ee739e0780",
+    "url": "https://afzalRapidInnovation.github.io/task-management-frontend/static/js/2.59890f25.chunk.js"
+  },
+  {
+    "revision": "5a9cc36b6888c0511aaf",
+    "url": "https://afzalRapidInnovation.github.io/task-management-frontend/static/js/main.4db49682.chunk.js"
+  },
+  {
+    "revision": "b59986d8db7882e65d77",
+    "url": "https://afzalRapidInnovation.github.io/task-management-frontend/static/js/runtime~main.24f9c8f2.js"
+  }
+]);
